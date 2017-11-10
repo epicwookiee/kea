a079be24ee14cc2da13ff27a5cf38d42eaf08530
Aron Aebersold <epicwookiee@gmail.com>
2017-11-09 19:54:15 -0500

    slideshow
    fixed slideshow

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # kea
+Welcome to our travel blog! This site will contain small snippets from our
+travels to places around Louisville and the surrounding areas. We will also
+include our favorite things from our trips to Walt Disney World, including our
+favorite foods, rides, and special events. Our shop link will not be working at
+this time, but in the future will link to an Etsy page with shirts, stickers,
+and all other things designed by us! Thank you for taking the time to share this
+adventure with us!
--- kea.js
+++ kea.js
@@ -25,26 +25,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-/*nav*/
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive a.icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
